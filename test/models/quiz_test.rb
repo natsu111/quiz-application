cfd989a93650c5c15eb5762939f2c96827d6be9b
EC2 Default User <ec2-user@ip-172-31-33-221.ap-northeast-1.compute.internal>
2019-06-04 07:15:34 +0000

    13.1.3

--- test/models/quiz_test.rb
+++ test/models/quiz_test.rb
@@ -4,4 +4,28 @@ class QuizTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:michael)
+    # このコードは慣習的に正しくない
+    @quiz = Quiz.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @quiz.valid?
+  end
+
+  test "user id should be present" do
+    @quiz.user_id = nil
+    assert_not @quiz.valid?
+  end
+  
+   test "content should be present" do
+    @quiz.content = "   "
+    assert_not @quiz.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @quiz.content = "a" * 141
+    assert_not @quiz.valid?
+  end
 end
