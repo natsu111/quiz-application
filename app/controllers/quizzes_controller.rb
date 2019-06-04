a3a89f0054c2f2a6fc4bee973fe088b4337391de
EC2 Default User <ec2-user@ip-172-31-33-221.ap-northeast-1.compute.internal>
2019-06-04 11:00:47 +0000

    Add user microposts binh thuong

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,5 +1,6 @@
 class QuizzesController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
+    before_action :correct_user,   only: :destroy
 
   def create
     @quiz = current_user.quizzes.build(quiz_params)
@@ -13,11 +14,19 @@ class QuizzesController < ApplicationController
   end
 
   def destroy
+    @quiz.destroy
+    flash[:success] = "Quiz deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def quiz_params
       params.require(:mquiz).permit(:content, :answer)
+    end
+    
+    def correct_user
+      @quiz = current_user.quizzes.find_by(id: params[:id])
+      redirect_to root_url if @quiz.nil?
     end
 end
--- test/controllers/quizzes_controller_test.rb
+++ test/controllers/quizzes_controller_test.rb
@@ -4,4 +4,39 @@ class QuizzesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  # def setup
+  #   @quiz = quizzes(:orange)
+  # end
+
+  # test "should redirect create when not logged in" do
+  #   assert_no_difference 'Quiz.count' do
+  #     post quizzes_path, params: { quiz: { content: "Lorem ipsum" } }
+  #   end
+  #   assert_redirected_to login_url
+  # end
+
+  # test "should redirect destroy when not logged in" do
+  #   assert_no_difference 'Quiz.count' do
+  #     delete quiz_path(@quiz)
+  #   end
+  #   assert_redirected_to login_url
+  # end
+
+  # test "should redirect destroy for wrong quiz" do
+  #   log_in_as(users(:michael))
+  #   quiz = quizzes(:ants)
+  #   assert_no_difference 'Quiz.count' do
+  #     delete quiz_path(quiz)
+  #   end
+  #   assert_redirected_to root_url
+  # end
+  
+  # test "should redirect destroy for wrong quiz" do
+  #   log_in_as(users(:michael))
+  #   quiz = quizzes(:ants)
+  #   assert_no_difference 'Quiz.count' do
+  #     delete quiz_path(quizzes)
+  #   end
+  #   assert_redirected_to root_url
+  # end
 end
