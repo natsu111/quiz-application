12cfaf5c2d0a8e3b8198a7d098f953b65291bb7a
EC2 Default User <ec2-user@ip-172-31-33-221.ap-northeast-1.compute.internal>
2019-06-05 05:14:36 +0000

    made answer panel

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,6 +1,8 @@
 class QuizzesController < ApplicationController
     before_action :logged_in_user, only: [:create, :destroy]
     before_action :correct_user,   only: :destroy
+    
+   
 
   def create
     @quiz = current_user.quizzes.build(quiz_params)
